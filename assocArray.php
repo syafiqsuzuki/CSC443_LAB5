bc146e90a7863f25542635d2b116197a0bd6263b
Syafiqm <syafiq4000@gmail.com>
2022-05-10 23:00:48 +0800

    fifth.commit

---
+++ assocArray.php
@@ -0,0 +1,19 @@
+SmonthDays = array ('January' => 31, February' => 28,
+
+'March' => 31, 'April' =>
+
+'May' 'July'
+
+=> 31, 'June'
+
+=> 31,
+
+=> 31, 'August'
+
+30,
+
+=> 30,
+
+September' => 30, 'October' => 31,
+
+'November' => 30, 'December' => 31);
