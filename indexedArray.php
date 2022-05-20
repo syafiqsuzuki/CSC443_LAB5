17b2b92a93815924c114aad650ac3fad6a687493
Syafiqm <syafiq4000@gmail.com>
2022-05-20 20:16:01 +0800

    third_commit

--- indexedArray.php
+++ indexedArray.php
@@ -1,5 +1,23 @@
-$month= array ('January', 'February', 'March', 'April', 'May', 'June', 'July',
-
-'August', 'September',
-
-'October', 'November', 'December');
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Basic PHP Syntax
+        </title>
+    </head>
+    <body>
+        <?php
+            $month = array ('January', 'February', 'March', 'April',
+                            'May', 'June', 'July', 'August', 'September',
+                            'October', 'November', 'December');
+            for($i = 0; $i < 12; $i++){
+                echo $month[$i], "\t";
+            };
+            echo "<br><br>";
+            sort($month, SORT_STRING);
+            foreach($month as $x){
+                echo $x, "<br>";
+            }
+        ?>
+    </body>
+</html>
