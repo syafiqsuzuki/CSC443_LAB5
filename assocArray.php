73a1206d6b4f90fbcc4bb4890d047df37fe7303c
Syafiqm <syafiq4000@gmail.com>
2022-05-20 20:13:52 +0800

    second_commit

--- assocArray.php
+++ assocArray.php
@@ -1,19 +1,30 @@
-SmonthDays = array ('January' => 31, February' => 28,
-
-'March' => 31, 'April' =>
-
-'May' 'July'
-
-=> 31, 'June'
-
-=> 31,
-
-=> 31, 'August'
-
-30,
-
-=> 30,
-
-September' => 30, 'October' => 31,
-
-'November' => 30, 'December' => 31);
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Basic PHP Syntax
+        </title>
+    </head>
+    <body>
+        <?php
+            $monthDays = array ('January' => 31, 'February' => 28,
+                                'March' => 31, 'April' => 30,
+                                'May' => 31, 'June' => 30,
+                                'July' => 31, 'August' => 31,
+                                'September' => 30, 'October' => 31,
+                                'November' => 30, 'December' => 31);
+            $month30 = array();
+            echo "<h2>Months around the year</h2><table border='1'><tr><th>Month</th><th>Days</th></tr>";
+            foreach($monthDays as $x => $xval){
+                echo "<tr><td>".$x."</td><td>".$xval."</td></tr>";
+                if($xval == 30){
+                    array_push($month30, $x);
+                }
+            }
+            echo "</table><br><br><h2>Months with 30 days</h2>";
+            foreach($month30 as $v){
+                echo $v."<br>";
+            }
+        ?>
+    </body>
+</html>
